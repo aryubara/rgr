50dd76f7903656a491c7e40f52818494e8ddc88f
Schmidt-dot <comedian.poliandra@gmail.com>
2026-06-12 22:24:41 +0700

    fileManager update

--- Utilities/fileManager.h
+++ Utilities/fileManager.h
@@ -4,5 +4,6 @@
 #include<string>
 #include<cstdint>
 
+//ИНИЦИАЛИЗАЦИЯ ФУНКЦИЙ
 std::vector<uint8_t> readFile(const std::string& path);
 void writeFile(const std::string& path, const std::vector<uint8_t>& inputData);
