76f10dbde2d1ea1a690939dd52e7690cba9c3071
Alina-Nerbysheva <a.nerbysheva@mail.ru>
2026-06-20 02:11:55 +0700

    Update Serpent.cpp

--- Serpent/Serpent.cpp
+++ Serpent/Serpent.cpp
@@ -28,177 +28,146 @@ const unsigned char invS[8][16] = {
     { 3, 0, 6,13, 9,14,15, 8, 5,12,11, 7,10, 1, 4, 2}
 };
 
-void linear_transform(uint32_t& x0, uint32_t& x1, uint32_t& x2, uint32_t& x3) {
-    x0 = (x0 << 13) | (x0 >> 19);
-    x2 = (x2 << 3) | (x2 >> 29);
-    x1 = x1 ^ x0 ^ x2;
-    x3 = x3 ^ x2 ^ (x0 << 3);
-    x1 = (x1 << 1) | (x1 >> 31);
-    x3 = (x3 << 7) | (x3 >> 25);
-    x0 = x0 ^ x1 ^ x3;
-    x2 = x2 ^ x3 ^ (x1 << 7);
-    x0 = (x0 << 5) | (x0 >> 27);
-    x2 = (x2 << 22) | (x2 >> 10);
+void linear_transform(uint32_t& word0, uint32_t& word1, uint32_t& word2, uint32_t& word3) {
+    word0 = ROTL(word0, 13); word2 = ROTL(word2, 3);
+    word1 ^= word0 ^ word2; word3 ^= word2 ^ (word0 << 3);
+    word1 = ROTL(word1, 1); word3 = ROTL(word3, 7);
+    word0 ^= word1 ^ word3; word2 ^= word3 ^ (word1 << 7);
+    word0 = ROTL(word0, 5); word2 = ROTL(word2, 22);
 }
 
-void inverse_linear_transform(uint32_t& x0, uint32_t& x1, uint32_t& x2, uint32_t& x3) {
-    x2 = (x2 << 10) | (x2 >> 22);
-    x0 = (x0 << 27) | (x0 >> 5);
-    x2 = x2 ^ x3 ^ (x1 << 7);
-    x0 = x0 ^ x1 ^ x3;
-    x3 = (x3 << 25) | (x3 >> 7);
-    x1 = (x1 << 31) | (x1 >> 1);
-    x3 = x3 ^ x2 ^ (x0 << 3);
-    x1 = x1 ^ x0 ^ x2;
-    x2 = (x2 << 29) | (x2 >> 3);
-    x0 = (x0 << 19) | (x0 >> 13);
+void inverse_linear_transform(uint32_t& word0, uint32_t& word1, uint32_t& word2, uint32_t& word3) {
+    word2 = ROTL(word2, 10); word0 = ROTL(word0, 27);
+    word2 ^= word3 ^ (word1 << 7); word0 ^= word1 ^ word3;
+    word3 = ROTL(word3, 25); word1 = ROTL(word1, 31);
+    word3 ^= word2 ^ (word0 << 3); word1 ^= word0 ^ word2;
+    word2 = ROTL(word2, 29); word0 = ROTL(word0, 19);
 }
 
-void sbox(uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d, int round) { // S-блочный слой
-    int si = round % 8;
-    for (int i = 0; i < 32; i += 4) {
-        unsigned int aa = (a >> i) & 0xF;
-        unsigned int bb = (b >> i) & 0xF;
-        unsigned int cc = (c >> i) & 0xF;
-        unsigned int dd = (d >> i) & 0xF;
+void sbox(uint32_t& word0, uint32_t& word1, uint32_t& word2, uint32_t& word3, int round) { // S-блочный слой
+    int sbox_index = round % 8;
+    for (int bit_pos = 0; bit_pos < 32; bit_pos += 4) {
+        unsigned int nibble0 = (word0 >> bit_pos) & 0xF; unsigned int nibble1 = (word1 >> bit_pos) & 0xF;
+        unsigned int nibble2 = (word2 >> bit_pos) & 0xF; unsigned int nibble3 = (word3 >> bit_pos) & 0xF;
         
-        aa = S[si][aa];
-        bb = S[si][bb];
-        cc = S[si][cc];
-        dd = S[si][dd];
+        nibble0 = S[sbox_index][nibble0]; nibble1 = S[sbox_index][nibble1];
+        nibble2 = S[sbox_index][nibble2]; nibble3 = S[sbox_index][nibble3];
         
-        a = (a & ~(0xF << i)) | (aa << i);
-        b = (b & ~(0xF << i)) | (bb << i);
-        c = (c & ~(0xF << i)) | (cc << i);
-        d = (d & ~(0xF << i)) | (dd << i);
+        word0 = (word0 & ~(0xF << bit_pos)) | (nibble0 << bit_pos);
+        word1 = (word1 & ~(0xF << bit_pos)) | (nibble1 << bit_pos);
+        word2 = (word2 & ~(0xF << bit_pos)) | (nibble2 << bit_pos);
+        word3 = (word3 & ~(0xF << bit_pos)) | (nibble3 << bit_pos);
     }
 }
 
-void inverse_sbox(uint32_t& a, uint32_t& b, uint32_t& c, uint32_t& d, int round) {
-    int si = round % 8;
-    for (int i = 0; i < 32; i += 4) {
-        unsigned int aa = (a >> i) & 0xF;
-        unsigned int bb = (b >> i) & 0xF;
-        unsigned int cc = (c >> i) & 0xF;
-        unsigned int dd = (d >> i) & 0xF;
+void inverse_sbox(uint32_t& word0, uint32_t& word1, uint32_t& word2, uint32_t& word3, int round) {
+    int sbox_index = round % 8;
+    for (int bit_pos = 0; bit_pos < 32; bit_pos += 4) {
+        unsigned int nibble0 = (word0 >> bit_pos) & 0xF; unsigned int nibble1 = (word1 >> bit_pos) & 0xF;
+        unsigned int nibble2 = (word2 >> bit_pos) & 0xF; unsigned int nibble3 = (word3 >> bit_pos) & 0xF;
         
-        aa = invS[si][aa];
-        bb = invS[si][bb];
-        cc = invS[si][cc];
-        dd = invS[si][dd];
+        nibble0 = invS[sbox_index][nibble0]; nibble1 = invS[sbox_index][nibble1];
+        nibble2 = invS[sbox_index][nibble2]; nibble3 = invS[sbox_index][nibble3];
         
-        a = (a & ~(0xF << i)) | (aa << i);
-        b = (b & ~(0xF << i)) | (bb << i);
-        c = (c & ~(0xF << i)) | (cc << i);
-        d = (d & ~(0xF << i)) | (dd << i);
+        word0 = (word0 & ~(0xF << bit_pos)) | (nibble0 << bit_pos);
+        word1 = (word1 & ~(0xF << bit_pos)) | (nibble1 << bit_pos);
+        word2 = (word2 & ~(0xF << bit_pos)) | (nibble2 << bit_pos);
+        word3 = (word3 & ~(0xF << bit_pos)) | (nibble3 << bit_pos);
     }
 }
 
 void serpent_key_schedule(const uint32_t key[8], uint32_t subkeys[132]) { // расширение ключа
-    uint32_t w[132];
+    uint32_t key_copy[132];
     const uint32_t PHI = 0x9E3779B9;
-    for (int i = 0; i < 8; i++) {
-        w[i] = key[i];
+    for (int key_ind = 0; key_ind < 8; ++key_ind) key_copy[key_ind] = key[key_ind];
+    for (int key_ind = 8; key_ind < 132; ++key_ind) {
+        key_copy[key_ind] = (key_copy[key_ind-8] ^ key_copy[key_ind-5] ^ key_copy[key_ind-3] ^ key_copy[key_ind-1] ^ PHI ^ (key_ind-8));
+        key_copy[key_ind] = (key_copy[key_ind] << 11) | (key_copy[key_ind] >> 21);
     }
-    for (int i = 8; i < 132; i++) {
-        w[i] = (w[i-8] ^ w[i-5] ^ w[i-3] ^ w[i-1] ^ PHI ^ (i-8));
-        w[i] = (w[i] << 11) | (w[i] >> 21);
-    }
-    for (int i = 0; i < 33; i++) {
-        subkeys[4*i] = w[4*i];
-        subkeys[4*i+1] = w[4*i+1];
-        subkeys[4*i+2] = w[4*i+2];
-        subkeys[4*i+3] = w[4*i+3];
+    for (int key_ind = 0; key_ind < 33; ++key_ind) {
+        subkeys[4*key_ind] = key_copy[4*key_ind]; subkeys[4*key_ind+1] = key_copy[4*key_ind+1];
+        subkeys[4*key_ind+2] = key_copy[4*key_ind+2]; subkeys[4*key_ind+3] = key_copy[4*key_ind+3];
         
-        int si = (33 - 1 - i) % 8;
-        for (int j = 0; j < 4; j++) {
-            unsigned int a = (subkeys[4*i+j] >> 0) & 0xF;
-            unsigned int b = (subkeys[4*i+j] >> 4) & 0xF;
-            unsigned int c = (subkeys[4*i+j] >> 8) & 0xF;
-            unsigned int d = (subkeys[4*i+j] >> 12) & 0xF;
-            unsigned int e = (subkeys[4*i+j] >> 16) & 0xF;
-            unsigned int f = (subkeys[4*i+j] >> 20) & 0xF;
-            unsigned int g = (subkeys[4*i+j] >> 24) & 0xF;
-            unsigned int h = (subkeys[4*i+j] >> 28) & 0xF;
+        int sbox_index = (32 - key_ind) % 8;
+        for (int word_ind = 0; word_ind < 4; ++word_ind) {
+            unsigned int nibble0 = (subkeys[4*key_ind+word_ind] >> 0) & 0xF; unsigned int nibble1 = (subkeys[4*key_ind+word_ind] >> 4) & 0xF;
+            unsigned int nibble2 = (subkeys[4*key_ind+word_ind] >> 8) & 0xF; unsigned int nibble3 = (subkeys[4*key_ind+word_ind] >> 12) & 0xF;
+            unsigned int nibble4 = (subkeys[4*key_ind+word_ind] >> 16) & 0xF; unsigned int nibble5 = (subkeys[4*key_ind+word_ind] >> 20) & 0xF;
+            unsigned int nibble6 = (subkeys[4*key_ind+word_ind] >> 24) & 0xF; unsigned int nibble7 = (subkeys[4*key_ind+word_ind] >> 28) & 0xF;
             
-            a = S[si][a];
-            b = S[si][b];
-            c = S[si][c];
-            d = S[si][d];
-            e = S[si][e];
-            f = S[si][f];
-            g = S[si][g];
-            h = S[si][h];
+            nibble0 = S[sbox_index][nibble0]; nibble1 = S[sbox_index][nibble1]; nibble2 = S[sbox_index][nibble2]; nibble3 = S[sbox_index][nibble3];
+            nibble4 = S[sbox_index][nibble4]; nibble5 = S[sbox_index][nibble5]; nibble6 = S[sbox_index][nibble6]; nibble7 = S[sbox_index][nibble7];
             
-            subkeys[4*i+j] = a | (b<<4) | (c<<8) | (d<<12) | (e<<16) | (f<<20) | (g<<24) | (h<<28);
+            subkeys[4*key_ind+word_ind] = nibble0 | (nibble0<<4) | (nibble0<<8) | (nibble0<<12) |
+             (nibble0<<16) | (nibble0<<20) | (nibble0<<24) | (nibble0<<28);
         }
     }
 }
 
 void serpent_encrypt_block(const uint8_t plaintext[16], uint8_t ciphertext[16], const uint32_t subkeys[132]) {
-    uint32_t x0, x1, x2, x3;
-    x0 = (plaintext[0] << 24) | (plaintext[1] << 16) | (plaintext[2] << 8) | plaintext[3];
-    x1 = (plaintext[4] << 24) | (plaintext[5] << 16) | (plaintext[6] << 8) | plaintext[7];
-    x2 = (plaintext[8] << 24) | (plaintext[9] << 16) | (plaintext[10] << 8) | plaintext[11];
-    x3 = (plaintext[12] << 24) | (plaintext[13] << 16) | (plaintext[14] << 8) | plaintext[15];
+    uint32_t state0, state1, state2, state3;
+    state0 = (plaintext[0] << 24) | (plaintext[1] << 16) | (plaintext[2] << 8) | plaintext[3];
+    state1 = (plaintext[4] << 24) | (plaintext[5] << 16) | (plaintext[6] << 8) | plaintext[7];
+    state2 = (plaintext[8] << 24) | (plaintext[9] << 16) | (plaintext[10] << 8) | plaintext[11];
+    state3 = (plaintext[12] << 24) | (plaintext[13] << 16) | (plaintext[14] << 8) | plaintext[15];
     
     for (int round = 0; round < 32; round++) {
-        x0 ^= subkeys[4*round];
-        x1 ^= subkeys[4*round+1];
-        x2 ^= subkeys[4*round+2];
-        x3 ^= subkeys[4*round+3];
+        state0 ^= subkeys[4*round];
+        state1 ^= subkeys[4*round+1];
+        state2 ^= subkeys[4*round+2];
+        state3 ^= subkeys[4*round+3];
         
-        sbox(x0, x1, x2, x3, round);
+        sbox(state0, state1, state2, state3, round);
         if (round < 31) {
-            linear_transform(x0, x1, x2, x3);
+            linear_transform(state0, state1, state2, state3);
         }
     }
-    x0 ^= subkeys[128];
-    x1 ^= subkeys[129];
-    x2 ^= subkeys[130];
-    x3 ^= subkeys[131];
+    state0 ^= subkeys[128];
+    state1 ^= subkeys[129];
+    state2 ^= subkeys[130];
+    state3 ^= subkeys[131];
     
-    ciphertext[0] = x0 >> 24; ciphertext[1] = x0 >> 16; ciphertext[2] = x0 >> 8; ciphertext[3] = x0;
-    ciphertext[4] = x1 >> 24; ciphertext[5] = x1 >> 16; ciphertext[6] = x1 >> 8; ciphertext[7] = x1;
-    ciphertext[8] = x2 >> 24; ciphertext[9] = x2 >> 16; ciphertext[10] = x2 >> 8; ciphertext[11] = x2;
-    ciphertext[12] = x3 >> 24; ciphertext[13] = x3 >> 16; ciphertext[14] = x3 >> 8; ciphertext[15] = x3;
+    ciphertext[0] = state0 >> 24; ciphertext[1] = state0 >> 16; ciphertext[2] = state0 >> 8; ciphertext[3] = state0;
+    ciphertext[4] = state1 >> 24; ciphertext[5] = state1 >> 16; ciphertext[6] = state1 >> 8; ciphertext[7] = state1;
+    ciphertext[8] = state2 >> 24; ciphertext[9] = state2 >> 16; ciphertext[10] = state2 >> 8; ciphertext[11] = state2;
+    ciphertext[12] = state3 >> 24; ciphertext[13] = state3 >> 16; ciphertext[14] = state3 >> 8; ciphertext[15] = state3;
 }
 
 void serpent_decrypt(const uint8_t ciphertext[16], uint8_t plaintext[16], // дешифрование блока
                      const uint32_t subkeys[132]) {
-    uint32_t x0, x1, x2, x3;
+    uint32_t state0, state1, state2, state3;
     
-    x0 = (ciphertext[0] << 24) | (ciphertext[1] << 16) | (ciphertext[2] << 8) | ciphertext[3];
-    x1 = (ciphertext[4] << 24) | (ciphertext[5] << 16) | (ciphertext[6] << 8) | ciphertext[7];
-    x2 = (ciphertext[8] << 24) | (ciphertext[9] << 16) | (ciphertext[10] << 8) | ciphertext[11];
-    x3 = (ciphertext[12] << 24) | (ciphertext[13] << 16) | (ciphertext[14] << 8) | ciphertext[15];
+    state0 = (ciphertext[0] << 24) | (ciphertext[1] << 16) | (ciphertext[2] << 8) | ciphertext[3];
+    state1 = (ciphertext[4] << 24) | (ciphertext[5] << 16) | (ciphertext[6] << 8) | ciphertext[7];
+    state2 = (ciphertext[8] << 24) | (ciphertext[9] << 16) | (ciphertext[10] << 8) | ciphertext[11];
+    state3 = (ciphertext[12] << 24) | (ciphertext[13] << 16) | (ciphertext[14] << 8) | ciphertext[15];
     
-    x0 ^= subkeys[128];
-    x1 ^= subkeys[129];
-    x2 ^= subkeys[130];
-    x3 ^= subkeys[131];
+    state0 ^= subkeys[128];
+    state1 ^= subkeys[129];
+    state2 ^= subkeys[130];
+    state3 ^= subkeys[131];
     
     for (int round = 31; round >= 0; round--) {
         if (round < 31) {
-            inverse_linear_transform(x0, x1, x2, x3);
+            inverse_linear_transform(state0, state1, state2, state3);
         }
-        inverse_sbox(x0, x1, x2, x3, round);
+        inverse_sbox(state0, state1, state2, state3, round);
         
-        x0 ^= subkeys[4*round];
-        x1 ^= subkeys[4*round+1];
-        x2 ^= subkeys[4*round+2];
-        x3 ^= subkeys[4*round+3];
+        state0 ^= subkeys[4*round];
+        state1 ^= subkeys[4*round+1];
+        state2 ^= subkeys[4*round+2];
+        state3 ^= subkeys[4*round+3];
     }
-    plaintext[0] = x0 >> 24; plaintext[1] = x0 >> 16; plaintext[2] = x0 >> 8; plaintext[3] = x0;
-    plaintext[4] = x1 >> 24; plaintext[5] = x1 >> 16; plaintext[6] = x1 >> 8; plaintext[7] = x1;
-    plaintext[8] = x2 >> 24; plaintext[9] = x2 >> 16; plaintext[10] = x2 >> 8; plaintext[11] = x2;
-    plaintext[12] = x3 >> 24; plaintext[13] = x3 >> 16; plaintext[14] = x3 >> 8; plaintext[15] = x3;
+    plaintext[0] = state0 >> 24; plaintext[1] = state0 >> 16; plaintext[2] = state0 >> 8; plaintext[3] = state0;
+    plaintext[4] = state1 >> 24; plaintext[5] = state1 >> 16; plaintext[6] = state1 >> 8; plaintext[7] = state1;
+    plaintext[8] = state2 >> 24; plaintext[9] = state2 >> 16; plaintext[10] = state2 >> 8; plaintext[11] = state2;
+    plaintext[12] = state3 >> 24; plaintext[13] = state3 >> 16; plaintext[14] = state3 >> 8; plaintext[15] = state3;
 }
 
 void addPadding(vector<uint8_t>& data) {
     uint8_t padding = 16 - (data.size() % 16);
     if (padding == 0) padding = 16;
-    for (uint8_t i = 0; i < padding; i++) {
+    for (uint8_t i = 0; i < padding; ++i) {
         data.push_back(padding);
     }
 }
@@ -228,8 +197,7 @@ vector<uint8_t> encryptData(const vector<uint8_t>& input, const uint32_t subkeys
 
 vector<uint8_t> decryptData(const vector<uint8_t>& encrypted, const uint32_t subkeys[132]) {
     vector<uint8_t> decrypted;
-    uint8_t block[16];
-    uint8_t out[16];
+    uint8_t block[16], out[16];
     
     for (size_t i = 0; i < encrypted.size(); i += 16) {
         memcpy(block, &encrypted[i], 16);
@@ -242,9 +210,7 @@ vector<uint8_t> decryptData(const vector<uint8_t>& encrypted, const uint32_t sub
 
 extern "C" {
 
-const char* getName() {
-    return "Serpent";
-}
+const char* getName() { return "Serpent"; }
 
 void encrypt(const vector<uint8_t>& input, vector<uint8_t>& output, const uint8_t* key) {
     uint32_t key_words[8];
